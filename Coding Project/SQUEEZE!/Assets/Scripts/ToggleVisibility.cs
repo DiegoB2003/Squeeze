564e94aa5565478adf4b85670be9b96c776a1f04
Nick Filipov <108493392+Nickick10@users.noreply.github.com>
2025-02-12 22:25:44 -0600

    Shop button connected to shop scroll view

---
+++ Coding Project/SQUEEZE!/Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ToggleVisibility : MonoBehaviour
+{
+    public GameObject targetObject; //Assigns target object to turn on/off
+
+    void Start()
+    {
+        if (targetObject != null)
+        {
+            targetObject.SetActive(false);
+        }
+    }
+
+    public void ToggleObject()
+    {
+        if (targetObject != null)
+        {
+            targetObject.SetActive(!targetObject.activeSelf);
+        }
+    }
+}
