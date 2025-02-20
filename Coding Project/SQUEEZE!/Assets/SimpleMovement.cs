b745381b8925286c36ae48756d0935fcb1fda07d
JoseB <103848847+JoseB29@users.noreply.github.com>
2025-02-20 10:54:41 -0600

    Added a temp background and moving customer
    Added some background and a duck customer that moves

---
+++ Coding Project/SQUEEZE!/Assets/SimpleMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public float speed = 5f; // Movement speed
+    public float maxDistance = 10f; // Distance before stopping
+    public Button TakeOrder; // Reference to the UI Button
+
+    private Rigidbody2D rb;
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = new Vector2(speed, 0);
+        rb.gravityScale = 0;
+        rb.freezeRotation = true;
+
+        startPosition = transform.position; // Store initial position
+
+        if (TakeOrder != null)
+        {
+            TakeOrder.gameObject.SetActive(false); // Hide button at the start
+            TakeOrder.onClick.AddListener(HideButton); // Attach function to button click
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float distanceTraveled = Vector2.Distance(startPosition, transform.position);
+
+        if (distanceTraveled >= maxDistance)
+        {
+            rb.linearVelocity = Vector2.zero; // Stop moving
+            if (TakeOrder != null)
+            {
+                TakeOrder.gameObject.SetActive(true); // Show the button
+            }
+        }
+    }
+
+    void HideButton()
+    {
+        if (TakeOrder != null)
+        {
+            TakeOrder.gameObject.SetActive(false); // Hide button when clicked
+        }
+    }
+}
