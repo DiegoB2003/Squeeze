7db2dd45b8602f13d2c258ba997582b6edc01f83
abrav2 <128727809+abrav2@users.noreply.github.com>
2025-04-18 11:36:23 -0500

    Change Price scene added

---
+++ Coding Project/SQUEEZE!/Assets/priceManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro; 
+public class priceManager : MonoBehaviour
+{
+    public TMP_InputField lemonInput;
+    public TMP_InputField sugarInput;
+    public TMP_InputField teaInput;
+    public TMP_InputField grapesInput;
+    public TMP_InputField lemonadeInput;
+    public TMP_InputField raspberryInput;
+    public TMP_InputField strawberryInput;
+    public TMP_InputField raspberryLemonadeInput;
+
+    void Start()
+    {
+       
+    }
+
+}
