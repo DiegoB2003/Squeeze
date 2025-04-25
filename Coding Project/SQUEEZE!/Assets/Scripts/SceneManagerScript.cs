b6e47701fb81f3d5d59a087b967ad874d973fcd1
dbravo4 <dbravo4@uic.edu>
2025-04-25 03:38:06 -0500

    added new item screen and made presentation build The new items screen shows new items obtained on unlockable days. I made a new build in the PresentationBuild folder for the presentation of course.

--- Coding Project/SQUEEZE!/Assets/Scripts/SceneManagerScript.cs
+++ Coding Project/SQUEEZE!/Assets/Scripts/SceneManagerScript.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public static SceneManagerScript Instance;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    void Awake()
+    {
+        Instance = this;
     }
 }
